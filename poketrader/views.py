6603fb394295dbdcb257c718cdc28c312eafe4a6
Adam Brandizzi <adam@brandizzi.com.br>
2021-02-10 10:00:48 -0300

    A little code formatting.

--- poketrader/views.py
+++ poketrader/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    pokemon_list1 = request.session.get('pokemon_list1', [])
-    pokemon_list2 = request.session.get('pokemon_list2', [])
+    session = request.session
+    pokemon_list1 = session.get('pokemon_list1', [])
+    pokemon_list2 = session.get('pokemon_list2', [])
+
     if request.method == 'POST':
         pokemon_set = request.POST['pokemon_set']
         pokemon_name = request.POST['pokemon_name']
@@ -14,6 +16,8 @@ def index(request):
             pokemon_list1.append(pokemon_name)
         elif pokemon_set == '2':
             pokemon_list2.append(pokemon_name)
-        request.session['pokemon_list1'] = pokemon_list1
-        request.session['pokemon_list2'] = pokemon_list2
+
+        session['pokemon_list1'] = pokemon_list1
+        session['pokemon_list2'] = pokemon_list2
+
     return render(request, 'index.html', {'pokemon_list1': pokemon_list1, 'pokemon_list2': pokemon_list2})
