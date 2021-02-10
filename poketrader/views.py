8a094bb220ade700e89c28f108811929389952ed
Adam Brandizzi <adam@brandizzi.com.br>
2021-02-09 21:45:37 -0300

    Save names in session

--- poketrader/views.py
+++ poketrader/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .forms import PokemonForm
-
 # Create your views here.
 
 
 def index(request):
-    form = PokemonForm()
-    return render(request, 'index.html', {'form': form})
+    pokemon_list1 = request.session.get('pokemon_list1', [])
+    pokemon_list2 = request.session.get('pokemon_list2', [])
+    if request.method == 'POST':
+        pokemon_set = request.POST['pokemon_set']
+        pokemon_name = request.POST['pokemon_name']
+        if pokemon_set == '1':
+            pokemon_list1.append(pokemon_name)
+        elif pokemon_set == '2':
+            pokemon_list2.append(pokemon_name)
+        request.session['pokemon_list1'] = pokemon_list1
+        request.session['pokemon_list2'] = pokemon_list2
+    return render(request, 'index.html', {'pokemon_list1': pokemon_list1, 'pokemon_list2': pokemon_list2})
