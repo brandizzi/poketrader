6dc098d3552e87f7851d1b5dec98be197583c8d3
Adam Brandizzi <adam@brandizzi.com.br>
2021-02-15 05:44:33 -0300

    Create a pokemon model

--- poketrader/models.py
+++ poketrader/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    base_experience = models.IntegerField()
+    picture_url = models.URLField()
